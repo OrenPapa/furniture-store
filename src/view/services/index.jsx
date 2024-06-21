51ea3a16a2d1001ef651cb1373c59a3b9500b23a
oren papa <o.papa.ext@teamsystem.com>
2024-06-21 12:26:15 +0200

    Add Placeholders for un-finished pages

--- src/view/services/index.jsx
+++ src/view/services/index.jsx
@@ -1,8 +1,25 @@
+import Footer from "../../components/footer";
+import Navbar from "../../components/navbar";
 
 function Services() {
   return (
-    <div>Services</div>
-  )
+    <div>
+      <Navbar />
+      <div
+        className="pt-28 w-full flex flex-col items-center"
+        style={{ minHeight: "calc(100vh - 112px)" }}
+      >
+        <div
+          style={{ minHeight: "calc(100vh - 512px)" }}
+          className="flex w-full flex-col justify-center items-center"
+        >
+          <div className="text-2xl mb-2">We are sorry!</div>
+          <div> This page is still in development</div>
+        </div>
+        <Footer />
+      </div>
+    </div>
+  );
 }
 
-export default Services
+export default Services;
